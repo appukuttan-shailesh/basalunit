00701f4a5e382b6dcc3def08d7cf9afdc8a111e6
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2018-04-24 15:45:36 +0200

    update version to 1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ json_files = package_files('basalunit/tests/somafeat_stim')
 
 setup(
     name='basalunit',
-    version='0.1',
+    version='1.0',
     author='Shailesh Appukuttan',
     author_email='shailesh.appukuttan@unic.cnrs-gif.fr',
     packages=['basalunit',
@@ -26,12 +26,11 @@ setup(
               'basalunit.plots'],
     package_data={'basalunit': json_files},
     url='https://github.com/appukuttan-shailesh/basalunit',
-    download_url = 'https://github.com/appukuttan-shailesh/basalunit/archive/0.1.tar.gz', 
     keywords = ['basal ganglia', 'electrical', 'efel', 'bluepyopt', 'validation framework'],
     license='MIT',
     description='A SciUnit library for data-driven testing of basal ganglia models.',
     long_description="",
-    install_requires=['neo','elephant','sciunit>=0.1.5.2',],
+    install_requires=['neo','elephant','sciunit>=0.1.5.2', 'tabulate'],
     dependency_links = ['git+http://github.com/neuralensemble/python-neo.git#egg=neo-0.4.0dev',
                         'https://github.com/scidash/sciunit/tarball/dev']
 )
