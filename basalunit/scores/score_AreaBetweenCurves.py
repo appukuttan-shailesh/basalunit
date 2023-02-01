b5ec93b39fa97d3e5f213b850af51c5642afd905
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2023-02-01 16:23:14 +0100

    Comments to similarity measures between 2D-curves from experiment and model

--- basalunit/scores/score_AreaBetweenCurves.py
+++ basalunit/scores/score_AreaBetweenCurves.py
@@ -8,6 +8,20 @@ class AreaInBetweenScore(sciunit.Score):
     """
     An Area in between two curves score. A float giving the value of
     a Area in between them between two 2D-curves.
+
+    Two curves are generally discretized into a time-series of ordered data points.
+    The algorithm constructs quadrilaterals between two curves and calculates
+    the area for each quadrilateral. Two curves that are being compared require
+    the same number of points in order to construct quadrilaterals to approximate
+    the total area between curves. If one curve has fewer data points than the
+    other curve, data points are added until both curves have the same number of
+    data points. It was chosen to add, rather than remove points to avoid any
+    loss of information for general problems.
+
+    Source: Jekel, C. F., Venter, G., Venter, M. P., Stander, N., & Haftka,
+    R. T. (2018). Similarity measures for identifying material parameters from
+    hysteresis loops using inverse analysis. International Journal of Material
+    Forming. https://doi.org/10.1007/s12289-018-1421-8
     """
 
     _allowed_types = (float,)
--- basalunit/scores/score_CurveLength.py
+++ basalunit/scores/score_CurveLength.py
@@ -8,6 +8,17 @@ class CurveLengthScore(sciunit.Score):
     """
     A Curve Length score. A float giving the value of
     a Curve Length between two 2D-curves.
+
+    A corresponding data point on one curve is calculated at the
+    equivalent curve length location of the other curve. Squared residual
+    values are then calculated as function of both coordinates. The sum of
+    these squared residuals is used to quantify the difference between
+    the two curves.
+
+    Source: Jekel, C. F., Venter, G., Venter, M. P., Stander, N., & Haftka,
+    R. T. (2018). Similarity measures for identifying material parameters from
+    hysteresis loops using inverse analysis. International Journal of Material
+    Forming. https://doi.org/10.1007/s12289-018-1421-8
     """
 
     _allowed_types = (float,)
